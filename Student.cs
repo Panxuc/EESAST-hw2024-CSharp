8defce8ece45a13456ec5397c06df2746a121261
yihan-wa <1572542354@qq.com>
2024-07-26 20:46:47 +0800

    Update Student.cs

--- Student.cs
+++ Student.cs
@@ -23,31 +23,6 @@ public interface IStudent
 
 public class Student : IStudent
 {
-    namespace StudentGradeManager;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-public interface IStudent
-{
-    public string Name { get; set; }
-    public int ID { get; set; }
-    public Dictionary<string, Grade> Grades { get; }
-    public void AddGrade(string course, string credit, string score);
-    public void AddGrades(List<(string course, int credit, int score)> grades);
-    public void RemoveGrade(string course);
-    public void RemoveGrades(List<string> courses);
-    public int GetTotalCredit();
-    public double GetTotalGradePoint();
-    public double GetGPA();
-    public string ToString();
-}
-
-public class Student : IStudent
-{
     private string name;
     private int id;
     private Dictionary<string, Grade> grades = new Dictionary<string, Grade>();
@@ -152,6 +127,4 @@ public class Student : IStudent
         }
         return result;
     }
-}
-
 }
