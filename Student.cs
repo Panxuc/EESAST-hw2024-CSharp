110bf782baeb24fe5538a2500aafd077dd09390e
SummerSanches <103926258+SummerSanches@users.noreply.github.com>
2024-08-01 23:19:47 +0800

    Update Student.cs

--- Student.cs
+++ Student.cs
@@ -23,46 +23,5 @@ public interface IStudent
 
 public class Student : IStudent
 {
-    public string Name { get; set; }
-    public int ID { get; set; }
-    public Dictionary<string, Grade> Grades { get; } = new Dictionary<string, Grade>();
-    public void AddGrade(string course, int credit, int score)
-    {
-        Grades[course] = new Grade(credit, score);
-    }
-    public void AddGrades(List<(string course, int credit, int score)> grades)
-    {
-        foreach (var (course, credit, score) in grades)
-        {
-            Grades[course] = new Grade(credit, score);
-        }
-    }
-    public void RemoveGrade(string course)
-    {
-        Grades.Remove(course);
-    }
-    public void RemoveGrades(List<string> courses)
-    {
-        foreach (var course in courses)
-        {
-            Grades.Remove(course);
-        }
-    }
-    public int GetTotalCredit()
-    {
-        return Grades.Values.Sum(g => g.Credit);
-    }
-    public double GetTotalGradePoint()
-    {
-        return Grades.Values.Sum(g => g.Credit * g.GradePoint);
-    }
-    public double GetGPA()
-    {
-        var totalCredits = GetTotalCredit();
-        return totalCredits > 0 ? GetTotalGradePoint() / totalCredits : 0.0;
-    }
-    public override string ToString()
-    {
-        return $"{Name} (ID: {ID}), GPA: {GetGPA():F2}";
-    }  
+    
 }
