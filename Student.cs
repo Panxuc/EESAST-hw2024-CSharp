9c39ede3638e59690268b74d25dbbd5f63dd4765
Hujh23 <hu-jh23@mails.tsinghua.edu.cn>
2024-08-12 10:23:57 +0800

    CSharp_胡纪亨_无35

--- Student.cs
+++ Student.cs
@@ -23,7 +23,59 @@ public interface IStudent
 
 public class Student : IStudent
 {
-    // 请仅在此处实现接口，不要在此处以外的地方进行任何修改
-    // 请尽可能周全地考虑鲁棒性
-    // 提交作业时请删除这 3 行注释
-}
+    private Dictionary<string, Grade> grades = new Dictionary<string, Grade>();
+    public string Name { get; set; }
+    public int ID { get; set; }
+    public Dictionary<string, Grade> Grades => grades;
+    public void AddGrade(string course, string credit, string score)
+        {
+            if (int.TryParse(credit, out int creditValue) && double.TryParse(score, out double scoreValue))
+            {
+                grades[course] = new Grade { Credit = creditValue, Score = scoreValue };
+            }
+            else
+            {
+                throw new ArgumentException("Invalid credit or score format.");
+            }
+        }
+    public void AddGrades(List<(string course, int credit, int score)> grades)
+        {
+            foreach (var grade in grades)
+            {
+                this.grades[grade.course] = new Grade { Credit = grade.credit, Score = grade.score };
+            }
+        }
+    public void RemoveGrade(string course)
+        {
+            grades.Remove(course);
+        }
+    public void RemoveGrades(List<string> courses)
+        {
+            foreach (var course in courses)
+            {
+                grades.Remove(course);
+            }
+        }
+    public int GetTotalCredit()
+        {
+            return grades.Values.Sum(g => g.Credit);
+        }
+    public double GetTotalGradePoint()
+        {
+            return grades.Sum(g => g.Value.Credit * g.Value.Score);
+        }
+    public double GetGPA()
+        {
+            int totalCredit = GetTotalCredit();
+            if (totalCredit == 0)
+            {
+                return 0.0;
+            }
+            return GetTotalGradePoint() / totalCredit;
+        }
+        public override string ToString()
+        {
+            var gradeInfo = string.Join(", ", grades.Select(g => $"{g.Key}: {g.Value.Score} (Credit: {g.Value.Credit})"));
+            return $"Student ID: {ID}, Name: {Name}, Grades: [{gradeInfo}]";
+        }
+    }
