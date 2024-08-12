b764c37040c2091206197e44978ab80576780e68
Hujh23 <hu-jh23@mails.tsinghua.edu.cn>
2024-08-12 12:59:37 +0800

    CSharp_胡纪亨_无35

--- Student.cs
+++ Student.cs
@@ -24,9 +24,11 @@ public interface IStudent
 public class Student : IStudent
 {
     private Dictionary<string, Grade> grades = new Dictionary<string, Grade>();
+
     public string Name { get; set; }
     public int ID { get; set; }
     public Dictionary<string, Grade> Grades => grades;
+
     public Student(string name, string id)
     {
         Name = name ?? throw new ArgumentNullException(nameof(name));
@@ -39,55 +41,64 @@ public class Student : IStudent
             throw new ArgumentException("Invalid ID format.");
         }
     }
+
     public void AddGrade(string course, string credit, string score)
+    {
+        if (int.TryParse(credit, out int creditValue) && double.TryParse(score, out double scoreValue))
         {
-            if (int.TryParse(credit, out int creditValue) && double.TryParse(score, out double scoreValue))
-            {
-                grades[course] = new Grade(creditValue,(int)scoreValue);
-            }
-            else
-            {
-                throw new ArgumentException("Invalid credit or score format.");
-            }
+            grades[course] = new Grade(creditValue, (int)scoreValue);
         }
+        else
+        {
+            throw new ArgumentException("Invalid credit or score format.");
+        }
+    }
+
     public void AddGrades(List<(string course, int credit, int score)> grades)
+    {
+        foreach (var grade in grades)
         {
-            foreach (var grade in grades)
-            {
-                this.grades[grade.course] = new Grade(grade.credit, grade.score);
-            }
+            this.grades[grade.course] = new Grade(grade.credit, grade.score);
         }
+    }
+
     public void RemoveGrade(string course)
+    {
+        grades.Remove(course);
+    }
+
+    public void RemoveGrades(List<string> courses)
+    {
+        foreach (var course in courses)
         {
             grades.Remove(course);
         }
-    public void RemoveGrades(List<string> courses)
-        {
-            foreach (var course in courses)
-            {
-                grades.Remove(course);
-            }
-        }
-    public int GetTotalCredit()
-        {
-            return grades.Values.Sum(g => g.Credit);
-        }
-    public double GetTotalGradePoint()
-        {
-            return grades.Sum(g => g.Value.Credit * g.Value.Score);
-        }
-    public double GetGPA()
-        {
-            int totalCredit = GetTotalCredit();
-            if (totalCredit == 0)
-            {
-                return 0.0;
-            }
-            return GetTotalGradePoint() / totalCredit;
-        }
-        public override string ToString()
-        {
-            var gradeInfo = string.Join(", ", grades.Select(g => $"{g.Key}: {g.Value.Score} (Credit: {g.Value.Credit})"));
-            return $"Student ID: {ID}, Name: {Name}, Grades: [{gradeInfo}]";
-        }
     }
+
+    public int GetTotalCredit()
+    {
+        return grades.Values.Sum(g => g.Credit);
+    }
+
+    public double GetTotalGradePoint()
+    {
+        return grades.Sum(g => g.Value.Credit * g.Value.Score);
+    }
+
+    public double GetGPA()
+    {
+        int totalCredit = GetTotalCredit();
+        if (totalCredit == 0)
+        {
+            return 0.0;
+        }
+        return GetTotalGradePoint() / totalCredit;
+    }
+
+    public override string ToString()
+    {
+        var gradeInfo = string.Join(", ", grades.Select(g => $"{g.Key}: {g.Value.Score} (Credit: {g.Value.Credit})"));
+        return $"Student ID: {ID}, Name: {Name}, Grades: [{gradeInfo}]";
+    }
+}
+
