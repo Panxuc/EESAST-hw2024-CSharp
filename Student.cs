20dcc226a1e9b516ed221c9f76fa3e052d29451f
violet <1749246846@qq.com>
2024-08-26 12:59:10 +0800

    x_x

--- Student.cs
+++ Student.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 public interface IStudent
 {
     public string Name { get; set; }
-    public int ID { get; set; }
+    public string ID { get; set; }
     public Dictionary<string, Grade> Grades { get; }
     public void AddGrade(string course, string credit, string score);
     public void AddGrades(List<(string course, int credit, int score)> grades);
@@ -23,7 +23,72 @@ public interface IStudent
 
 public class Student : IStudent
 {
-    // 请仅在此处实现接口，不要在此处以外的地方进行任何修改
-    // 请尽可能周全地考虑鲁棒性
-    // 提交作业时请删除这 3 行注释
+    public string Name { get; set; }
+    public string ID { get; set; }
+    public Dictionary<string, Grade> Grades { get; }
+
+    public Student(string name, string id)
+    {
+        Name = name;
+        ID = id;
+        Grades = new Dictionary<string, Grade>();
+    }
+
+    public void AddGrade(string course, string credit, string score)
+    {
+        Grades.Add(course, new Grade(credit, score));
+    }
+
+    public void AddGrades(List<(string course, int credit, int score)> grades)
+    {
+        foreach (var grade in grades)
+        {
+            Grades.Add(grade.course, new Grade(grade.credit.ToString(), grade.score.ToString()));
+        }
+    }
+
+    public void RemoveGrade(string course)
+    {
+        Grades.Remove(course);
+    }
+
+    public void RemoveGrades(List<string> courses)
+    {
+        foreach (var course in courses)
+        {
+            Grades.Remove(course);
+        }
+    }
+
+    public int GetTotalCredit()
+    {
+        return Grades.Values.Sum(grade => grade.Credit);
+    }
+
+    public double GetTotalGradePoint()
+    {
+        return Grades.Values.Sum(grade => grade.GradePoint*grade.Credit);
+    }
+
+    public double GetGPA()
+    {
+        return GetTotalGradePoint() / GetTotalCredit();
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"Name: {Name}");
+        sb.AppendLine($"ID: {ID}");
+        sb.AppendLine("Grades:");
+        foreach (var grade in Grades)
+        {
+            sb.AppendLine($"{grade.Key}: {grade.Value}");
+        }
+        sb.AppendLine($"Total Credit: {GetTotalCredit()}");
+        sb.AppendLine($"Total Grade Point: {GetTotalGradePoint()}");
+        sb.AppendLine($"GPA: {GetGPA()}");
+        return sb.ToString();
+    }
+
 }
