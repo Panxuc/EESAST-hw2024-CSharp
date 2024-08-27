27c8ad3eefe5e2644a9b6e4e480b26f3390db66d
dennis <dennis@mails.com>
2024-08-27 22:16:25 +0800

    CSharp_章诒和_无32

--- Student.cs
+++ Student.cs
@@ -25,63 +25,81 @@ public interface IStudent
 public class Student : IStudent
 {
     private string name;
-    public string Name{
-        get=>name;
-        set{
-            name=value;
+    public string Name
+    {
+        get => name;
+        set
+        {
+            name = value;
         }
     }
     private int id;
-    public int ID{
-        get=>id;
-        set{
-            id=value;
+    public int ID
+    {
+        get => id;
+        set
+        {
+            id = value;
         }
     }
-    public Student(string?n,string?i){
-        if(n!=null&&i!=null){
-            name=n;
-            id=Convert.ToInt32(i);
-            grade=new Dictionary<string, Grade>();
+    public Student(string n, string i)
+    {
+        if (n != null && i != null)
+        {
+            name = n;
+            id = Convert.ToInt32(i);
+            grade = new Dictionary<string, Grade>();
         }
     }
-    private Dictionary<string,Grade> grade;
-    public Dictionary<string,Grade> Grades{
-        get=>grade;
-        private set{
-            grade=value;
+    private Dictionary<string, Grade> grade;
+    public Dictionary<string, Grade> Grades
+    {
+        get => grade;
+        private set
+        {
+            grade = value;
         }
     }
-    public void AddGrade(string course, string credit, string score){
-        Grades.Add(course,new Grade(Convert.ToInt32(credit),Convert.ToInt32(score)));
+    public void AddGrade(string course, string credit, string score)
+    {
+        Grades.Add(course, new Grade(Convert.ToInt32(credit), Convert.ToInt32(score)));
     }
-    public void AddGrades(List<(string course, int credit, int score)> grades){
-        foreach(var i in grades){
-            Grades.Add(i.course,new Grade(i.credit,i.score));
+    public void AddGrades(List<(string course, int credit, int score)> grades)
+    {
+        foreach (var i in grades)
+        {
+            Grades.Add(i.course, new Grade(i.credit, i.score));
         }
     }
-    public void RemoveGrade(string course){
+    public void RemoveGrade(string course)
+    {
         Grades.Remove(course);
     }
-    public void RemoveGrades(List<string> courses){
-        foreach(var i in courses){
+    public void RemoveGrades(List<string> courses)
+    {
+        foreach (var i in courses)
+        {
             RemoveGrade(i);
         }
     }
-    public int GetTotalCredit(){
-        int total_credit=0;
-        foreach(var i in grade){
-            total_credit+=i.Value.Credit;
+    public int GetTotalCredit()
+    {
+        int total_credit = 0;
+        foreach (var i in grade)
+        {
+            total_credit += i.Value.Credit;
         }
         return total_credit;
     }
-    public double GetTotalGradePoint(){
-        double total_gradepoint=0;
-        foreach(var i in grade){
-            total_gradepoint+=i.Value.GradePoint*i.Value.Credit;
+    public double GetTotalGradePoint()
+    {
+        double total_gradepoint = 0;
+        foreach (var i in grade)
+        {
+            total_gradepoint += i.Value.GradePoint * i.Value.Credit;
         }
         return total_gradepoint;
     }
-    public double GetGPA()=>GetTotalGradePoint()/GetTotalCredit();
-    override public string ToString()=>$"Name:{name}\nID:{id}\nGPA:{GetGPA()}\n";
+    public double GetGPA() => GetTotalCredit() == 0 ? 0 : GetTotalGradePoint() / GetTotalCredit();
+    override public string ToString() => $"Name:{name}\nID:{id}\nGPA:{GetGPA()}\n";
 }
